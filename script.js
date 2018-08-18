c7238c8628768e2eeb45f4b77dd7df2e2d9e8cec
Unknown <jhample323@west-mec.org>
2018-08-17 17:38:17 -0700

    Final
    Cleaned up my code. Created all the code that creates input boxes red.

--- script.js
+++ script.js
@@ -7,21 +7,58 @@
 */
 
 "use strict";
-var formValidity = false;
+var formValidity = true;
 
 // function to validate form
 function validateForm(evt) {
-    if (evt.preventDefault) {
-        evt.preventDefault();
-    } else {
-        evt.returnValue = false;
+    formValidity = true;
+    validateRequired();
+    if (formValidity === false) {
+        if (evt.preventDefault) {
+            evt.preventDefault();
+        } else {
+            evt.returnValue = false;
+        }
+    }
+}
+
+// function that validates required
+function validateRequired() {
+    var inputElement = document.querySelectorAll("input");
+    var errorMsg = document.getElementById("errorText");
+    var fieldset = true;
+    try {
+        for (var i = 0; i < inputElement.length; i++) {
+            if (inputElement[i].value === "") {
+                fieldset = false;
+                inputElement[i].style.background = "rgb(255,233,233)";
+            } else {
+                inputElement[i].style.background = "white";
+            }
+        }
+        if (fieldset === false) {
+            throw "Please fill out the required fields"
+        }
+    } catch (msg) {
+        formValidity = false;
+        errorMsg.style.display = "block";
+        errorMsg.innerHTML = msg;
     }
 }
 
 //function to create event listeners
 function createEventListeners() {
-    window.addEventListener("submit", validateForm);
+    if (window.addEventListener) {
+        window.addEventListener("submit", validateForm, false);
+    } else if (window.attachEvent) {
+        window.attachEvent("onsubmit", validateForm);
+    }
+
 }
 
 // load event listener
-window.addEventListener("load", createEventListeners);
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
